81c861edd0bf6c1c1b1a751ce70f3edde661f248
chromafunk <xavier@eo10.net>
2023-05-08 19:55:40 +0000

    remove commented code and change apiVersion

--- src/uniswap-v-2-factory.ts
+++ src/uniswap-v-2-factory.ts
@@ -42,26 +42,3 @@ export function handlePairCreated(event: PairCreatedEvent): void {
 
 }
 
-// import { PairCreated as PairCreatedEvent } from "../generated/UniswapV2Factory/UniswapV2Factory"
-// import { PairCreated } from "../generated/schema"
-// import { BigInt } from "@graphprotocol/graph-ts"
-// import { Pair } from "../generated/schema"
-// import {UniswapV2Pair as PairTemplate} from "../generated/templates"
-
-// export function handlePairCreated(event: PairCreatedEvent): void {
-//   let entity = new PairCreated(
-//     event.transaction.hash.concatI32(event.logIndex.toI32())
-//   )
-//   entity.token0 = event.params.token0
-//   entity.token1 = event.params.token1
-//   entity.pair = event.params.pair
-//   entity.param3 = event.params.param3
-
-//   entity.blockNumber = event.block.number
-//   entity.blockTimestamp = event.block.timestamp
-//   entity.transactionHash = event.transaction.hash
-
-//   entity.save()
-// }
-
-
